ffa22873303127802efe7d39c8f5393d3ddb698b
Maiaguiter <49686906@est.ort.edu.ar>
2022-04-08 08:47:56 -0300

    Initial commit

---
+++ Assets/EJ2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ2 : MonoBehaviour
+{
+    public int preciouno;
+    public int preciodos;
+    public int preciotres;
+    public int precioproductos;
+    public int dinerodepersona;
+    
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int dinerofaltante;
+        int dinerosobrante;
+        precioproductos = preciouno + preciodos + preciotres;
+        if (precioproductos > dinerodepersona)
+        {
+            dinerofaltante = precioproductos - dinerodepersona;
+            Debug.Log("Falta dinero " + dinerofaltante + " para la compra"); 
+        }
+        else if (precioproductos < dinerodepersona)
+        {
+            dinerosobrante = dinerodepersona - precioproductos;
+            Debug.Log(" Sobra dinero " + dinerosobrante + " para la compra");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
